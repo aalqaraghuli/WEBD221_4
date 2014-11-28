20a1160c28cbf9acad2cadc6926049d849852d0f
hzlllns <hello@hzl.ca>
2014-11-28 09:55:15 -0500

    update

---
+++ hazel-llanes/footer.php
@@ -0,0 +1,5 @@
+	<footer>
+		<p>Copyright &copy; 2014 Studio New</p>
+	</footer><!-- /footer -->
+</body>
+</html>
---
+++ hazel-llanes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta content="A studio website" name="description">
+	<meta content="Hazel Llanes" name="author">
+	<title><?php $page_title ?></title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body class="home">
+	<nav>
+		<h1>Studio New</h1>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Portfolio</a></li>
+			<li><a href="#">Contact Us</a></li>
+		</ul>
+	</nav><!-- /nav -->
+
+	<header>
+		<!-- carousel! -->
+		<h2>Welcome to Studio New</h2>
+		<h3>We create websites and stuff</h3>
+	</header><!-- /header -->
